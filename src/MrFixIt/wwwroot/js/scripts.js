1a0f18bde4ad7e5ae479e33ce7d8d9f07f130467
Caitlin Hines <caitlinhines@me.com>
2017-05-05 10:05:17 -0700

    linked jquery and working on AJAX to display claim job

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,12 @@
 ﻿var test = "it works";
+
+$('.claim-job').click(function () {
+    $.ajax({
+        type: 'GET',
+        datatype: 'html',
+        url: $(this).data('request-url'),
+        success: function (result) {
+            $('.claim').html(result);
+        }
+    });
+ });
