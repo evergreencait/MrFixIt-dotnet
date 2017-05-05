53a760061a0d31d66a31e7b65f6b75cce50dedcd
Caitlin Hines <caitlinhines@me.com>
2017-05-05 10:59:35 -0700

    able to see if job is pending or complete on index page

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,7 +1,8 @@
 ﻿var test = "it works";
 
 $(document).ready(function () {
-
+    
+    //works to use ajax to claim a job on the index page
     $('.claim-job').click(function () {
         $.ajax({
             type: 'GET',
